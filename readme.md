ede80b7be5d132f69384b613120c15a369fb6780
jsheikhgenlongs <jsheikhgenlogs@genlogs.io>
2024-02-13 01:30:38 -0500

    first

---
+++ readme.md
@@ -0,0 +1,1 @@
+used these docs: https://developer.mozilla.org/en-US/docs/Web/CSS/background-image
---
+++ scripts.js
@@ -0,0 +1,13 @@
+function submitYes(){ 
+    document.getElementById("williamsmithington").style.visibility = "visible"
+    document.getElementById("yamama").style.visibility = "visible"
+}
+
+function submitNo(){ 
+    document.getElementById("lebussy").style.visibility = "visible";
+    document.getElementById("yamama").style.visibility = "visible"
+}
+
+function nyerm(){
+    window.location.href = "yamama.html"
+}
